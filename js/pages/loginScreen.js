6b8c56c307be45fc8b97ab49b979bb8890a32397
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-07 15:27:11 -0300

    Formating to prettier

--- js/pages/loginScreen.js
+++ js/pages/loginScreen.js
@@ -1,29 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-  loadPageElementsActions();
-  loadOauthConfigs();
-}, false);
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    loadPageElementsActions();
+    loadOauthConfigs();
+  },
+  false
+);
 
-
-const loadPageElementsActions = async() =>{
-
+const loadPageElementsActions = async () => {
   var chk_UsingTermsElement = document.getElementById("chk-terms");
 
   chk_UsingTermsElement.onchange = handleUsingTermsChkChange;
-
-}
+};
 
 const loadOauthConfigs = async () => {
   gLoginSetCalbacks(googleLoginOnSuccess, googleLoginOnError);
-}
+};
 
-googleLoginOnSuccess = (gData) =>{
-  document.getElementById('login-info').innerText = "Signed in: " + gData.getBasicProfile().getName();
+googleLoginOnSuccess = (gData) => {
+  document.getElementById("login-info").innerText =
+    "Signed in: " + gData.getBasicProfile().getName();
   console.log(gData.getBasicProfile());
-}
-googleLoginOnError = (err) =>{
+};
+googleLoginOnError = (err) => {
   console.log(err.error);
-}
+};
 
 handleUsingTermsChkChange = (e) => {
   console.log(e.target.checked);
-}
+};
--- js/pwa/serviceWorker.js
+++ js/pwa/serviceWorker.js
@@ -1,11 +1,11 @@
-self.addEventListener('install', event => {
-  console.log('Caminhada ServiceWorker install done!');
+self.addEventListener("install", (event) => {
+  console.log("Caminhada ServiceWorker install done!");
 });
 
-self.addEventListener('activate', event => {
-  console.log('Caminhada ServiceWorker activating done!');
+self.addEventListener("activate", (event) => {
+  console.log("Caminhada ServiceWorker activating done!");
 });
 
-self.addEventListener('fetch', event => {
-  console.log('Caminhada ServiceWorker fetching ', event.request.url);
+self.addEventListener("fetch", (event) => {
+  console.log("Caminhada ServiceWorker fetching ", event.request.url);
 });

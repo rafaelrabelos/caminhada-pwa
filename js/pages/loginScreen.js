83ec4890ffa1ec1bf0fd68be8bedd35a2f44d0d1
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-07 00:56:09 -0300

    Adding google Oauth to login button

--- js/pages/loginScreen.js
+++ js/pages/loginScreen.js
@@ -2,18 +2,34 @@ document.addEventListener('DOMContentLoaded', function() {
   loadPageElementsActions();
 }, false);
 
+
 const loadPageElementsActions = () =>{
 
   var btn_LoginElement = document.getElementById("btn-login");
   var chk_UsingTermsElement = document.getElementById("chk-terms");
 
-  btn_LoginElement.onclick = handleLoginBtnClick;
   chk_UsingTermsElement.onchange = handleUsingTermsChkChange;
 
 }
 
-handleLoginBtnClick = (e) => {
-  console.log(e.target.id);
+ window.attachGoogleLoginToElement = () => {
+  gapi.load('auth2', () => {
+    var btn_LoginElement = document.getElementById("btn-login");
+    auth2 = gapi.auth2.init({
+      client_id: '222678136207-db4poveolpkr5301j4gj37evlekkctq9.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin',
+    });
+  
+    auth2.attachClickHandler(btn_LoginElement, {}, googleLoginOnSuccess, googleLoginOnError);
+  });
+}
+
+googleLoginOnSuccess = (gData) =>{
+  document.getElementById('login-info').innerText = "Signed in: " + gData.getBasicProfile().getName();
+  console.log(gData.getBasicProfile());
+}
+googleLoginOnError = (err) =>{
+  console.log(err.error);
 }
 
 handleUsingTermsChkChange = (e) => {

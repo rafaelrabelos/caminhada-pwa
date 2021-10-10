6fa7237f28e20a85f110aacb68fa938de1d0c9dc
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-10 18:56:51 -0300

    Add cache to service worker

--- js/pwa/serviceWorker.js
+++ js/pwa/serviceWorker.js
@@ -1,5 +1,18 @@
+let cache_name = 'caminhada-v0.15';
+let urls_to_cache = [
+  '/',
+  '/js/pages',
+  '/js/services',
+  '/js/utils'
+ ]
+
 self.addEventListener("install", (event) => {
   console.log("Caminhada ServiceWorker install done!");
+
+  event.waitUntil(caches.open(cache_name).then((cache) => {
+    return cache.addAll(urls_to_cache)
+   }) )
+
 });
 
 self.addEventListener("activate", (event) => {
@@ -8,4 +21,11 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   console.log("Caminhada ServiceWorker fetching ", event.request.url);
+  
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) return response;
+      else return fetch(event.request);
+    })
+  );
 });

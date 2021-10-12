d27d62dc21224f9fc26ecfc34d1fdb5d0b6ff62e
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-12 17:54:59 -0300

    updates static cache

--- js/pwa/serviceWorker.js
+++ js/pwa/serviceWorker.js
@@ -1,9 +1,15 @@
 let cache_name = 'caminhada-v0.15';
 let urls_to_cache = [
   '/',
-  '/js/pages',
-  '/js/services',
-  '/js/utils'
+  '/index.html',
+  '/walking.html',
+  '/js/pages/loginScreen.js',
+  '/js/pages/walking',
+  '/js/pwa/config.js',
+  '/js/services/walkingService.js',
+  '/js/utils/gLoginOauth2.js',
+  '/js/utils/session.js',
+  '/styles/main.css'
  ]
 
 self.addEventListener("install", (event) => {

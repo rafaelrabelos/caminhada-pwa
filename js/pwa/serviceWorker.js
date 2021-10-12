5e3122a9cd6f5c56dad3fadedb92ade82530a019
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-12 20:12:20 -0300

    Aditional files to cache

--- js/pwa/serviceWorker.js
+++ js/pwa/serviceWorker.js
@@ -1,4 +1,4 @@
-let cache_name = 'caminhada-v0.15';
+let cache_name = 'caminhada-v0.16';
 let urls_to_cache = [
   '/',
   '/index.html',
@@ -6,10 +6,26 @@ let urls_to_cache = [
   '/js/pages/loginScreen.js',
   '/js/pages/walking.js',
   '/js/pwa/config.js',
+  '/js/pwa/serviceWorker.js',
   '/js/services/walkingService.js',
   '/js/utils/gLoginOauth2.js',
   '/js/utils/session.js',
-  '/styles/main.css'
+  '/styles/main.css',
+  'https://apis.google.com/js/platform.js',
+  'https://apis.google.com/js/platform.js?onload=attachGoogleLogin',
+  '/assets/imagens/icones/android/mipmap-xxxhdpi/ic_launcher.png',
+  '/assets/imagens/icones/favicon/favicon.ico',
+  '/assets/imagens/icones/IOS/29.png',
+  '/assets/imagens/icones/IOS/40.png',
+  '/assets/imagens/icones/IOS/57.png',
+  '/assets/imagens/icones/IOS/58.png',
+  '/assets/imagens/icones/IOS/60.png',
+  '/assets/imagens/icones/IOS/80.png',
+  '/assets/imagens/icones/IOS/87.png',
+  '/assets/imagens/icones/IOS/114.png',
+  '/assets/imagens/icones/IOS/120.png',
+  '/assets/imagens/icones/IOS/180.png',
+  '/assets/imagens/icones/IOS/1024.png',
  ]
 
 self.addEventListener("install", (event) => {

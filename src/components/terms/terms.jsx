53a9ad321cb7b03ff619f5d66b8706716703c487
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-23 02:50:59 -0300

    Creates terms and conditions component

---
+++ src/components/terms/terms.jsx
@@ -0,0 +1,33 @@
+import { __TEXTS, __LANG } from "../../utils/constants";
+import "./logo.css";
+
+const TermsAndConditions = (props) => {
+  const texts = __TEXTS[__LANG].loginView;
+  const { onAccept, onChange, checked } = props;
+
+  const handleOnChange = (e) => {
+    onChange(e.target.checked);
+    if (e.target.checked && onAccept) onAccept();
+  };
+
+  return (
+    <div className="texts-small">
+      <input
+        id="chk-terms"
+        type="checkbox"
+        onChange={handleOnChange}
+        checked={checked || false}
+      />
+      <small>
+        {`${texts.acceptTerms.accept} `}
+        <b className="texts-orange texts-small">{`${texts.acceptTerms.terms} `}</b>{" "}
+        e
+        <b className="texts-orange texts-small">
+          {` ${texts.acceptTerms.policies} `}
+        </b>
+      </small>
+    </div>
+  );
+};
+
+export default TermsAndConditions;

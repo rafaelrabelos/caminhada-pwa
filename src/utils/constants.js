e4b634ffb5de8b7a92382db5f4f28beae35efd3e
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-23 02:48:00 -0300

    Creates data constants

---
+++ src/utils/constants.js
@@ -0,0 +1,27 @@
+export const getLang = () => "ptbr";
+export const __LANG = getLang();
+
+export const __CONSTS = {
+  orientations: { horizontal: 1, vertical: -1 },
+  sizes: { sm: 20, smx: 25, md: 30, mdx: 35, xg: 40, xgx: 45, full: 100 },
+};
+
+export const __TEXTS = {
+  ptbr: {
+    titleApp: `Meus privilegios`,
+    titlePage: `Caminhada do privilégio`,
+    buttons:{
+      enter: `Entrar`,
+      start: `Iniciar`
+    },
+    loginView:{
+      presentation: `Responda algumas questões e descubra sua posição de privilégio social.`,
+      acceptToStart: `Para iniciar a caminhada aceite os termos e condições.`,
+      acceptTerms: {
+        accept: `Eu aceito os`,
+        terms: `Termos de Uso`,
+        policies: `Politica de Privacidade`,
+      }
+    }
+  },
+};

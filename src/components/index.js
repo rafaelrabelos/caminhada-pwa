6ada38a1f61b0a14ec87e0ba9dd844e8de48421e
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-23 02:51:23 -0300

    exports components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Logo from './logo/logo';
+import TermsAndConditions from './terms/terms';
+import {GoogleLoginButton, StartButton} from './buttons/buttons';
+
+export {
+  Logo, TermsAndConditions, GoogleLoginButton, StartButton
+} 

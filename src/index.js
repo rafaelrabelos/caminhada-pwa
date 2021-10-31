95e4afaf695951a87502f480d2279b2f4e8d47cb
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-31 00:35:50 -0300

    Lint refactor

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/styles/index.css";
 import { App } from "./views";
-import * as serviceWorkerRegistration from './config/pwa/serviceWorkerRegistration';
-import reportWebVitals from './config/testing/reportWebVitals';
+import * as serviceWorkerRegistration from "./config/pwa/serviceWorkerRegistration";
+import reportWebVitals from "./config/testing/reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals( (r) => console.log(r));
+reportWebVitals((r) => console.log);

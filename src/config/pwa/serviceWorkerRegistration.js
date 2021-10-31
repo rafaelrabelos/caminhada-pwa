913cea1c161b25eb004b25b583a73f9d44277285
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-30 23:18:22 -0300

    Fix missing serviceWorker

--- src/config/pwa/serviceWorkerRegistration.js
+++ src/config/pwa/serviceWorkerRegistration.js
@@ -30,12 +30,12 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/pwa/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
-
+        
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {

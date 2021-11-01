8d83ea9da5ccac68f54535121b7e75f9e08d950a
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-10-31 21:17:12 -0300

    Remove old files that was migated

--- js/pwa/config.js
+++
@@ -1,7 +0,0 @@
-try {
-  navigator.serviceWorker.register("./js/pwa/serviceWorker.js").then((reg) => {
-    console.log("Caminhada ServiceWorker registered");
-  });
-} catch (error) {
-  console.log(error);
-}
